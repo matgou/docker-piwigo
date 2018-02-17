e0e2bf62b361c69a8aa29154bcc06cad61601352
Mathieu GOULIN <mathieu.goulin@gadz.org>
2018-02-17 12:12:51 +0000

    create new user ldap

--- t.php
+++ t.php
@@ -8,8 +8,8 @@ $config['ld_use_ssl'] = False;
 $config['ld_bindpw'] = $_ENV['LDAP_BIND_DN'];
 $config['ld_binddn'] = $_ENV['LDAP_BIND_PW'];
 
-$config['allow_newusers'] = False;
-$config['advertise_admin_new_ldapuser'] = False;
+$config['allow_newusers'] = True;
+$config['advertise_admin_new_ldapuser'] = True;
 $config['send_password_by_mail_ldap'] = False;
 
 echo serialize($config);

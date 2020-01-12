8944c79789b0a7973b416cc0b998e2f7376c628c
Mathieu GOULIN <mathieu.goulin@gadz.org>
2020-01-12 18:10:16 +0000

    update Ldap_login plugin autogen

--- t.php
+++ t.php
@@ -1,4 +1,5 @@
 <?php
+//a:17:{"forgot_url";s:0:"";s:9:"ld_server";s:2:"ad";s:8:"ld_group";s:0:"";s:14:"ld_group_class";s:5:"group";s:22:"ld_group_member_attrib";s:6:"member";s:10:"ldap_debug";b:0;s:11:"ld_anonbind";b:0;}
 $config = array();
 $config['host'] = $_ENV['LDAP_HOST'];
 $config['basedn'] = $_ENV['LDAP_BASE_DN'];
@@ -11,6 +12,13 @@ $config['ld_binddn'] = $_ENV['LDAP_BIND_DN'];
 $config['allow_newusers'] = True;
 $config['advertise_admin_new_ldapuser'] = True;
 $config['send_password_by_mail_ldap'] = False;
+$config['forgot_url'] = "";
+$config['ld_server'] = "ad";
+$config['ld_group'] = "ld_server";
+$config['ld_group_class'] = "group";
+$config['ld_group_member_attrib'] = "member";
+$config['ldap_debug'] = False;
+$config['ld_anonbind'] = False;
 
 echo serialize($config);
 exit();
